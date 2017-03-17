d2576270580751eceb6fac5eddda605d8b14579b
Lavender-Ayodi-Andela <layodi749@gmail.com>
2017-03-17 18:23:33 +0300

    Complete reallocation functionality and add tests

--- test/test_amity.py
+++ test/test_amity.py
@@ -6,17 +6,29 @@ class AmityTest(unittest.TestCase):
     def setUp(self):
         self.Amity = Amity()
 
+
+    def _create_rooms(self):
+        room_types = {"office": ['MOMBASA', 'HOGWARTS', 'LAGOS'],
+        "livingspace": ['KENYA', 'PLATFORM', 'VALHALLA']
+        }
+        keys = room_types.keys()
+        for room in keys:
+            for names in room_types.get(room):
+                self.Amity.assign_room_name(room, names)
+
     def test_create_room_without_room(self):
         room_created = self.Amity.create_room("", 'LivingSpace')
-        self.assertEqual(room_created, "Provide both room name and room type.")
+        self.assertIn("Error", room_created)
 
     def test_create_room_without_type(self):
         room_created = self.Amity.create_room("Platform", "")
-        self.assertEqual(room_created, "Provide both room name and room type.")
+        self.assertIn("Error", room_created)
 
     def test_room_created_exists(self):
+        self.Amity.create_room('Lagos', 'office')
         room_created = self.Amity.create_room('Lagos', 'office')
-        self.assertEqual(room_created, "This room already exists.")
+        print (self.Amity.rooms)
+        self.assertEqual(room_created, "This room already exists")
 
     def test_amity_room_object(self):
         rooms = self.Amity.rooms.keys()
@@ -46,24 +58,69 @@ class AmityTest(unittest.TestCase):
         self.assertEqual(person_added, "Please insert a name.")
 
     def test_add_person_exists(self):
+        self._create_rooms()
+        self.Amity.add_person('LESLEY AYODI', 'staff', 'Y')
         person_added = self.Amity.add_person('LESLEY AYODI', 'staff', 'Y')
         self.assertEqual(person_added, "Name already exists. Choose another name.")
 
     def test_add_person_to_room(self):
-        person_added = self.Amity.add_person('EMILY MBELENGA', 'staff', 'Y')
-        self.assertEqual(person_added, "Lagos")
+        self._create_rooms()
+        room_allocated = self.Amity.add_person('EMILY MBELENGA', 'staff', 'N')
+        room_name = room_allocated["office"]
+        list_of_occupants = self.Amity.rooms.get("office")[room_name]
+        self.assertIn("EMILY MBELENGA", list_of_occupants)
 
-    def test_person_already_allocated(self):
-        person_allocated = self.Amity.reallocate_person('EMILY MBELENGA', 'Lagos', 6)
-        self.assertEqual(person_allocated, "Person has not yet been allocated a room.")
+    def test_check_occupants(self):
+        self.Amity.create_room('MOMBASA', 'office')
+        self.Amity.add_person('LESLEY AYODI', 'staff', 'N')
+        self.Amity.add_person('LAVENDER AYODI', 'fellow', 'N')
+        self.Amity.add_person('PATIENCE AYODI', 'staff', 'N')
+        room_occupants =  self.Amity._find_room_occupant('office', 'MOMBASA')
+        self.assertEqual(len(room_occupants), 3)
 
-    # def test_if_room_has_space(self):
-    #     room_space = self.Amity.reallocate_person('ISABELA WAKESHO', 'Lagos', 6)
-    #     self.assertEqual(room_space, "No space available in this room")
+    def test_invalid_room_type(self):
+        self.Amity.create_room('MOMBASA', 'office')
+        self.Amity.add_person('LESLEY AYODI', 'staff', 'N')
+        room_occupants =  self.Amity._find_room_occupant('offic', 'MOMBASA')
+        self.assertIn("Error", room_occupants)
 
-    # def test_check_occupants_name(self):
-    #     pace = self.Amity.reallocate_person('LESLEY AYODI', 'Lagos', 6)
-    #     self.assertEqual(pace, "LESLEY AYODI")
+    def test_invalid_room_name(self):
+        self.Amity.create_room('MOMBASA', 'office')
+        self.Amity.add_person('LESLEY AYODI', 'staff', 'N')
+        room_occupants =  self.Amity._find_room_occupant('office', 'MOMBA')
+        self.assertIn("Error", room_occupants)
+
+    def test_reallocate_person(self):
+        self.Amity.create_room('MOMBASA', 'office')
+        self.Amity.add_person('LESLEY AYODI', 'staff', 'N')
+        self.Amity.add_person('LAVENDER AYODI', 'staff', 'N')
+        self.Amity.create_room('LAGOS', 'office')
+        reallocate_person = self.Amity.reallocate_person("office", 'MOMBASA', 'LAGOS', 'LAVENDER AYODI')
+        self.assertEqual(True,reallocate_person)
+
+    def test_reallocate_invalid_person(self):
+        self.Amity.create_room('MOMBASA', 'office')
+        self.Amity.create_room('LAGOS', 'office')
+        reallocate_person = self.Amity.reallocate_person("office", 'MOMBASA', 'LAGOS', 'ISABEL AYODI')
+        self.assertIn('Error', reallocate_person)
+
+    def test_reallocate_invalid_room(self):
+        invalid_room = self.Amity.reallocate_person("office", 'MOMBASA', 'LAGOS', 'ISABEL AYODI')
+        self.assertIn('Error', invalid_room)
+
+    def test_full_room(self):
+        self.Amity.create_room('LAGOS', 'office')
+        self.Amity.add_person('LESLEY AYODI', 'staff', 'N')
+        self.Amity.create_room('MOMBASA', 'office')
+        occupants = ['A', 'B', 'C', 'D', 'E', 'F']
+        office = self.Amity.rooms.get('office')
+        office['MOMBASA'] = occupants
+        reallocate_person = self.Amity.reallocate_person("office", 'LAGOS', 'MOMBASA', 'LESLEY AYODI')
+        self.assertEqual(False, reallocate_person)
+
+
+
+
 
 
 
